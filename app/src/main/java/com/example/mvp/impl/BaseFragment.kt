8b7f941645987bd0aa39cdd98ea1b2433b62d1f5
xingjiyuan <xingjyuan@gmail.com>
2019-06-01 13:21:20 +0800

    add createPresenterKt

--- app/src/main/java/com/example/mvp/impl/BaseFragment.kt
+++ app/src/main/java/com/example/mvp/impl/BaseFragment.kt
@@ -2,15 +2,34 @@ package com.example.mvp.impl
 
 import android.support.v4.app.Fragment
 import com.example.mvp.IMvpView
+import com.example.mvp.IPresenter
+import kotlin.reflect.KClass
+import kotlin.reflect.full.isSubclassOf
+import kotlin.reflect.full.primaryConstructor
+import kotlin.reflect.jvm.jvmErasure
 
 class BaseFragment<out P : BasePresenter<BaseFragment<P>>> : IMvpView<P>, Fragment() {
 
     override val presenter: P
 
     init {
-        presenter = createPresenter()
+        presenter = createPresenterKt()
         presenter.view = this
     }
 
-    fun createPresenter(): P = TODO()
+    fun createPresenterKt(): P {
+        sequence {
+            var thisClass: KClass<*> = this@BaseFragment::class
+            while (true) {
+                yield(thisClass.supertypes)
+                thisClass = thisClass.supertypes.firstOrNull()?.jvmErasure ?: break
+            }
+        }.flatMap {
+            it.flatMap { it.arguments }.asSequence()
+        }.first {
+            it.type?.jvmErasure?.isSubclassOf(IPresenter::class) ?: false
+        }.let {
+            return it.type!!.jvmErasure.primaryConstructor!!.call() as P
+        }
+    }
 }
